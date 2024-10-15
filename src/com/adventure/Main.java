a1b12f043f2be45aee5098189502171c0c47f938
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-10-15 15:44:56 +0200

    load ui from main

--- src/com/adventure/Main.java
+++ src/com/adventure/Main.java
@@ -3,19 +3,7 @@ package com.adventure;
 public class Main {
   public static void main(String[] args) {
 
-    Environment env = new Environment();
-    env.loadPlaces(); // loads places with descriptions
-
-//    env.generateMap(5, 5);
-//    env.printMap();
-//    env.saveMap();
-//
-//    env.earthQuake();
-    env.loadMapFromFile();
-    env.printMap();
-
-    env.printCurrentPlace();
-
-    env.printSurroundingPlaces();
+    UI ui = new UI();
+    ui.menu();
   }
 }
