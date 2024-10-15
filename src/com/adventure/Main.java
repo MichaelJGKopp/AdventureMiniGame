e957f73c0ebc6c57440f834e32d5bc532eb2ea1e
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-10-15 12:19:45 +0200

    use map utility in main

--- src/com/adventure/Main.java
+++ src/com/adventure/Main.java
@@ -3,6 +3,12 @@ package com.adventure;
 public class Main {
   public static void main(String[] args) {
 
+    Environment env = new Environment();
+    env.loadData();
+    env.generateMap(5, 5);
+    env.printMap();
+    env.saveMap();
 
+    env.earthQuake();
   }
 }
